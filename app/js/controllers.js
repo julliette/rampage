e3d5a9482f07ab522c23a56b4f0f70b656cffded
StevenChin <schin@medullan.com>
2013-07-18 14:33:26 -0400

    Auto update task list, reset form data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,36 +14,42 @@ angular.module('rampage.controllers', ['rampage.services'])
 
 }).controller('MyCtrl2',function MyCtrl2($scope, kinvey) {
 	//Service call to retrieve data from Kinvey service
-	$scope.message = "";
+	$scope.message = "No tasks are present";
+	$scope.hasData = "";
 		
 	kinvey.getData().then(function(data){
-		$scope.tasks = data.data;
-		$scope.message = "No tasks are present";
+		
+		$scope.tasks = data.data;		
 		if ($scope.tasks.length > 0){
-			console.log("Tasks are present");
+			//console.log("Tasks are present");
 			$scope.hasData = true;
 		}
-	})
+		else
+			$scope.hasData = false;
+	})	
 	
 	$scope.createTask = function(newTask){
 		//console.log('post data');
-		var date = new Date();
-		var dateInMilliseconds = date.getTime();
+		var dateInMilliseconds = new Date().getTime();
+		$scope.newTask.CreatedDate = dateInMilliseconds;
 		var jsonTask = {
 			Content: newTask.Content,
 			Status: newTask.Status,
 			CreatedDate: dateInMilliseconds  
 		};
 		kinvey.postData(jsonTask);
+		$scope.tasks.push(newTask);
 		window.alert('The task ' + jsonTask.Content + ' has been saved!');
-		$scope.showOverlay = false;
-		//$route.reload();
-		//location.reload(false);
+		$scope.hideOverlay();
 	}
 	
-	$scope.refreshPage = function() {
-		console.log("Refresh page");
-		browser().navigateTo('#/viewTasks');
+	$scope.displayOverlay = function() {
+		document.getElementById("newTaskForm").reset();
+		$scope.showOverlay = true;
+	}
+	
+	$scope.hideOverlay = function() {
+		$scope.showOverlay = false;
 	}
 	
 	
