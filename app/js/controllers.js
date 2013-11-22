bcfd33b8c2e65ace3dd741b34e37932d17825d9e
Adesh Maraj <amaraj@medullan.com>
2013-11-22 13:44:03 -0400

    Working pagination on task list.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,8 +19,13 @@ function() {
 }])
 
 .controller('TaskListCtrl', function TaskListCtrl($scope, kinvey) {
+
+    $scope.itemsPerPage = 10;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
 	kinvey.task().then(function(data) {
 		$scope.tasks = data.data;
+		$scope.groupToPages();
 	}, function(data) {
 		$scope.data = data || "Request failed";
 	});
@@ -78,6 +83,47 @@ function() {
 			$scope.tasks = data.data;
 		});
  	};
+ 	
+ 	//Pagination attempt ...it not working >:(
+ 	$scope.groupToPages = function () {
+        $scope.pagedItems = [];
+        
+        for (var i = 0; i < $scope.tasks.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.tasks[i] ];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.tasks[i]);
+            }
+        }
+    };
+    
+    $scope.range = function (start, end) {
+        var ret = [];
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    };
+    
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+    
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+    
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
 })
 
 
